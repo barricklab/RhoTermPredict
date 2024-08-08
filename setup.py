817c6073403d9d8ac26a2a9af85d77f85cb3af71
croots <38054423+croots@users.noreply.github.com>
2024-08-08 13:55:47 -0500

    3.4 -> Dependency fix

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name="rhotermpredict",
-    version="3.3",
+    version="3.4",
     packages=["rhotermpredict"],
     package_dir={"rhotermpredict": "rhotermpredict"},
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=["numpy >= 1.15.4", "Bio >= 0.1.0"],
+    install_requires=["numpy >= 1.15.4", "biopython >= 0.1.0"],
     # metadata to display on PyPI
     author="Cameron Roots",
     author_email="croots@utexas.edu",
-    description="Barrick Lab fork of Salvo 2019 RhoTermPredict",
+    description="RhoTermPredict (Barrick Lab Fork)",
     keywords="",
     url="https://github.com/barricklab/RhoTermPredict",  # project home page, if any
     project_urls={
